25217c022ad582343c0df458ff575fd033a7c4f7
Manthan Gupta <guptamanthan280@gmail.com>
2023-12-11 02:03:19 +0530

    Anagram Solution updated

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,35 @@
 
 function isAnagram(str1, str2) {
 
+  if(str1.length !== str2.length){
+    console.log("They cannot be anagram");
+    return false;
+  }
+
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  let frequencyCount = {}
+  for(let i=0; i<str1.length ; i++){
+    let char = str1[i];
+    frequencyCount[char] = (frequencyCount[char] || 0) + 1;
+  }
+  
+  for(let i=0; i<str2.length; i++){
+    let char = str2[i];
+    frequencyCount[char] = (frequencyCount[char] || 0) - 1;
+  }
+
+  for(let char in frequencyCount){
+    if (frequencyCount[char]!== 0){
+      console.log("They are not Anagram")
+      return false;
+    }
+  }
+
+  console.log("They are Anagram");
 }
+
+isAnagram("hello","!hello")
 
 module.exports = isAnagram;
