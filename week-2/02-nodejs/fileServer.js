7bca53f7cd551ed05fcfe5d4a74072163a5d0434
manthangupta26 <102374120+manthangupta26@users.noreply.github.com>
2023-12-21 15:20:19 +0530

    fileServer Solution - all routes added

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -18,26 +18,30 @@ const path = require('path');
 const app = express();
 
 app.get('/files',(req,res)=>{
-  fs.readdir(path.join(__dirname,'./files/'),'utf-8',(err,files)=>{
+  fs.readdir(path.join(__dirname,'./files/'),(err,files)=>{
     if(err){
-      return res.status(500).json({err : 'Failed to retrieve file'})
+      return res.status(500).json({err : 'Failed to retrieve file'});
     }
-    res.json(files)
-  })
-})
+    res.json(files);
+  });
+});
 
 
 app.get('/file/:filename',(req,res) =>{
-  const pathname = path.join(__dirname,'./files/',req.params.filename)
+  const pathname = path.join(__dirname,'./files/',req.params.filename);
   fs.readFile(pathname,'utf-8',(err,data)=>{
     if(err){
-      return res.status(500).json({ err : 'Failed to retrieve file'})
+      return res.status(404).json({ err : 'Failed to retrieve file'})
     }
     res.json({
       data
-    })
-  })
-})
+    });
+  });
+});
+
+app.all('*',(req,res)=>{
+  res.status(404).send('Route not found');
+});
 
 app.listen(3000);
 
